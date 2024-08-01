bc62744be50d587b3727d1ae4ac394f12b277f1f
Renan Augusto dos Santos <146970438+renanfrontend@users.noreply.github.com>
2024-08-01 04:19:43 -0300

    update Login.tsx

--- src/Login.tsx
+++ src/Login.tsx
@@ -8,7 +8,7 @@ import { Titulo } from './componentes/Titulo';
 export default function Login({ navigation }) {
   return (
     <VStack flex={1} alignItems="center" justifyContent="center" p={5}>
-      <Image source={Logo} width={25} alt="Logo Voll" />
+      <Image source={Logo} alt="Logo Voll" />
 
       <Titulo>
         Faça login em sua conta
