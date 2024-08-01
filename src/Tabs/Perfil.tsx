f1d0216ca8c26adf147eb8292c187501cef62964
Renan Augusto dos Santos <146970438+renanfrontend@users.noreply.github.com>
2024-08-01 03:12:07 +0000

    update profile

--- src/Tabs/Perfil.tsx
+++ src/Tabs/Perfil.tsx
@@ -7,17 +7,18 @@ export default function Perfil(){
       <VStack flex={1} alignItems="center" p={5}>
         <Titulo color="blue.500">Meu Perfil</Titulo>
 
-        <Avatar size="xl" source={{ uri: "https://github.com/andreocunha.png" }} mt={5} />
+        <Avatar size="xl" source={{ uri: "https://github.com/renanfrontend" }} mt={5} />
 
         <Titulo color="blue.500">Informações pessoais</Titulo>
-        <Titulo fontSize="lg" mb={1}>André Cunha</Titulo>
-        <Text>12/12/1990</Text>
+        <Titulo fontSize="lg" mb={1}>Renan Augusto</Titulo>
+        <Text>20/01/1984</Text>
         <Text>São Paulo</Text>
 
         <Divider mt={5} />
 
         <Titulo color="blue.500" mb={1}>Histórico médico</Titulo>
-        <Text>Bronquite</Text>
+        <Text>Rinite Alérgica</Text>
+        <Text>TDAH</Text>
         <Text>Sinusite</Text>
       </VStack>
     </ScrollView>
